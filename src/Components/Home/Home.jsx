5c2188bf367a36efc817626327af7a359bed8828
AbdurRob <abdurrobr34l@gmail.com>
2025-11-03 18:28:18 +0600

    Created api route and showing lateset products

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '../Container/Container';
+import LatestProductSection from './LatestProductSection';
 
 const Home = () => {
+  // const latestProducts = fetch("http://localhost:3000/latest-products").then(res => res.json());
+    const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/latest-products")
+      .then((res) => res.json())
+      .then((data) => setLatestProducts(data))
+      .catch((err) => console.error("Error fetching products:", err));
+  }, []);
+
   return (
     <main className='commonSectionPadding pt-0!'>
       <title>Smart Deals | Home</title>
 
       <Container>
-        <h1>Lorem ipsum dolor sit.</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim veniam odio.</p>
+       <div>
+        <LatestProductSection latestProducts={latestProducts}></LatestProductSection>
+       </div>
       </Container>
     </main>
   );
