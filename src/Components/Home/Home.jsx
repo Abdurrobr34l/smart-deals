508e2e51c24c7a9685ebdc015479edfcac464bf2
AbdurRob <abdurrobr34l@gmail.com>
2025-11-03 13:32:18 +0600

    Style layout for whole website

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,17 +3,12 @@ import Container from '../Container/Container';
 
 const Home = () => {
   return (
-    <main>
+    <main className='commonSectionPadding pt-0!'>
       <title>Smart Deals | Home</title>
 
       <Container>
         <h1>Lorem ipsum dolor sit.</h1>
-        <p className='h-[600px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim veniam odio.</p>
-        <p className='h-[600px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim veniam odio.</p>
-        <p className='h-[600px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim veniam odio.</p>
-        <p className='h-[600px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim veniam odio.</p>
-        <p className='h-[600px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim veniam odio.</p>
-        <p className='h-[600px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim veniam odio.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus enim veniam odio.</p>
       </Container>
     </main>
   );
--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -8,7 +8,7 @@ const RootLayout = () => {
     <>
       <Header></Header>
       
-      <div className="mb-10 min-h-[calc(100vh-(92px+52px+150px))] bg-base-100 md:mb-14 lg:mb-20 border"> {/*//* 140px = header(92px) + footer(52px) */}
+      <div className="commonSectionPadding mb-10 min-h-[calc(100vh-(92px+52px))] bg-base-100 md:mb-14 lg:mb-20"> {/*//* 140px = header(92px) + footer(52px) */}
         <Outlet></Outlet>
       </div>
       
