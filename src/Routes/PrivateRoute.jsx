929b5cdedda5c69ee003a63dc860dd490e61d861
AbdurRob <abdurrobr34l@gmail.com>
2025-11-05 00:20:37 +0600

    Navigate path changed

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -15,7 +15,7 @@ const PrivateRoute = ({ children }) => {
   }
 
   if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    return <Navigate to="/register" state={{ from: location }} replace />;
   }
 
   return children;
